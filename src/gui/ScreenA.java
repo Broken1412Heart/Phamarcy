1e20d2edaff8feca19e32798db05615837852707
ngotanphat <ngotanphatyasu09092004@gmail.com>
2024-03-12 17:20:05 +0700

    modifies file a

--- src/gui/ScreenA.java
+++ src/gui/ScreenA.java
@@ -1,3 +1,15 @@
 public class ScreenA {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 
+    public static void main(String[] args) {
+        // Tạo và hiển thị giao diện
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ScreenMainOfManager();
+            }
+        });
+    
 }
