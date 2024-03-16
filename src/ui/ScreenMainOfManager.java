eeebfe99321616e252b06981d871a30daf76f102
Harry <congthanh30102001@gmail.com>
2024-03-16 07:38:02 +0700

    add Screen and entities

--- src/ui/ScreenMainOfManager.java
+++ src/ui/ScreenMainOfManager.java
@@ -1,4 +1,4 @@
-package gui;
+package ui;
 
 import javax.swing.*;
 import java.awt.*;
@@ -7,8 +7,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class ScreenMainOfEmployee extends JFrame {
-    public ScreenMainOfEmployee() {
+public class ScreenMainOfManager extends JFrame {
+    public ScreenMainOfManager() {
         // Thiết lập cửa sổ chính
         setTitle("Trung Tâm Quản Lý");
         setSize(800, 600); // Kích thước cửa sổ
@@ -40,7 +40,8 @@ public class ScreenMainOfEmployee extends JFrame {
 
         // Tạo menu bar
         JMenuBar menuBar = new JMenuBar();
-        String[] mainMenuNames = { "Quản lý thuốc", "Quản lý khách hàng", "Quản lý hoá đơn", "Báo cáo thống kê" };
+        String[] mainMenuNames = { "Quản lý thuốc", "Quản lý nhân viên", "Quản lý khách hàng", "Quản lý hoá đơn",
+                "Báo cáo thống kê" };
         for (String name : mainMenuNames) {
             JMenu menu = new JMenu(name);
             menu.addMouseListener(new MouseAdapter() {
@@ -66,7 +67,7 @@ public class ScreenMainOfEmployee extends JFrame {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new ScreenMainOfEmployee();
+                new ScreenMainOfManager();
             }
         });
     }
