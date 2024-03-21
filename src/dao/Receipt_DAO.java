0410c6c49472596a913a2e52897007abdc448c12
Harry <congthanh30102001@gmail.com>
2024-03-21 09:34:41 +0700

    add method DAO file

--- src/dao/Receipt_DAO.java
+++ src/dao/Receipt_DAO.java
@@ -1,5 +1,19 @@
 package dao;
 
 public class Receipt_DAO {
+    public void insertReceipt() {
+        // code here
+    }
 
+    public void updateReceipt() {
+        // code here
+    }
+
+    public void deleteReceipt() {
+        // code here
+    }
+
+    public void getReceipt() {
+        // code here
+    }
 }
